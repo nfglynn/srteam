a24e332efa55d068ff74c6b7a9a9d9edd41e37ec
Niall Glynn <nglynn@gmail.com>
2015-06-15 13:12:57 +0100

    Asyncio working now.

--- srteam/__init__.py
+++ srteam/__init__.py
@@ -1,4 +1,6 @@
 import requests
+import asyncio
+import aiohttp
 import m3u8
 import os
 
@@ -8,25 +10,51 @@ class Playlist():
         self._playlist_uri = requests.utils.urlparse(playlist_uri)
         self.base_uri = '{}/{}'.format(self._playlist_uri.hostname,
                                        os.path.dirname(self._playlist_uri.path))
-        self.base_name = os.path.basename(self._playlist_uri.path)
+        self.base_name = os.path.basename(self._playlist_uri.path).replace('.m3u8', '')
         self.uri = self._playlist_uri.geturl()
         try:
             resp = requests.get(self.uri)
         except Exception:
             raise Exception("Bad URI")
         try:
-            self.playlist = m3u8.M3U8(resp.text, base_uri=self.base_uri)
+            self._playlist = m3u8.M3U8(resp.text, base_uri=self.base_uri)
+            self._segments = self._playlist.segments
         except Exception:
             raise Exception("Bad playlist")
 
+    def _fetch_segment(self, segment, semaphore, connector):
+        data = None
+        for retry in range(10):
+            with (yield from semaphore):
+                try:
+                    uri = 'http://' + segment.absolute_uri
+                    print('FETCH', segment.uri)
+                    response = yield from aiohttp.request('GET',
+                                                          uri,
+                                                          connector=connector)
+                except Exception as exc:
+                    pass
+                else:
+                    data = yield from response.read()
+                    response.close()
+                    break
+        return data
+
+    def _fetch(self, connector):
+        semaphore = asyncio.Semaphore(10)
+        tasks = [asyncio.Task(self._fetch_segment(s, semaphore, connector)) for s in self._segments]
+        return (yield from asyncio.gather(*tasks))
+
     def save(self, path=None):
         if path is None:
-            path = os.path.join(os.getcwd(), self.base_name.replace('.m3u8', ''))
+            path = os.path.join(os.getcwd(), self.base_name)
+        loop = asyncio.get_event_loop()
+        connector = aiohttp.TCPConnector(share_cookies=True, loop=loop)
+        data = loop.run_until_complete(self._fetch(connector))
+        import ipdb; ipdb.set_trace()
         with open(path, 'wb') as f:
-            for segment in self.playlist.segments:
-                print('.', end='')
-                resp = requests.get('http://' + segment.absolute_uri)
-                f.write(resp.content)
+            for d in data:
+                f.write(d)
 
 
 if __name__ == "__main__":
